6c36db14fd8b8325f5f4cf7740a866621e965905
HKunogi <hima.kunogi@hotmail.com>
2022-09-17 12:58:08 -0300

    kcp src with 64 bit conv id

---
+++ GameServer/KcpSharp/KcpSocketTransport.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace KcpSharp
+{
+
+    /// <summary>
+    /// Helper methods to create socket transports for KCP conversations.
+    /// </summary>
+    public static class KcpSocketTransport
+    {
+        /// <summary>
+        /// Create a socket transport for KCP covnersation.
+        /// </summary>
+        /// <param name="socket">The socket instance.</param>
+        /// <param name="endPoint">The remote endpoint.</param>
+        /// <param name="conversationId">The conversation ID.</param>
+        /// <param name="options">The options of the <see cref="KcpConversation"/>.</param>
+        /// <returns>The created socket transport instance.</returns>
+        public static IKcpTransport<KcpConversation> CreateConversation(Socket socket, EndPoint endPoint, int conversationId, KcpConversationOptions? options)
+        {
+            if (socket is null)
+            {
+                throw new ArgumentNullException(nameof(socket));
+            }
+            if (endPoint is null)
+            {
+                throw new ArgumentNullException(nameof(endPoint));
+            }
+
+            return new KcpSocketTransportForConversation(socket, endPoint, conversationId, options);
+        }
+
+        /// <summary>
+        /// Create a socket transport for KCP covnersation with no conversation ID.
+        /// </summary>
+        /// <param name="socket">The socket instance.</param>
+        /// <param name="endPoint">The remote endpoint.</param>
+        /// <param name="options">The options of the <see cref="KcpConversation"/>.</param>
+        /// <returns>The created socket transport instance.</returns>
+        public static IKcpTransport<KcpConversation> CreateConversation(Socket socket, EndPoint endPoint, KcpConversationOptions? options)
+        {
+            if (socket is null)
+            {
+                throw new ArgumentNullException(nameof(socket));
+            }
+            if (endPoint is null)
+            {
+                throw new ArgumentNullException(nameof(endPoint));
+            }
+
+            return new KcpSocketTransportForConversation(socket, endPoint, null, options);
+        }
+
+        /// <summary>
+        /// Create a socket transport for raw channel.
+        /// </summary>
+        /// <param name="socket">The socket instance.</param>
+        /// <param name="endPoint">The remote endpoint.</param>
+        /// <param name="conversationId">The conversation ID.</param>
+        /// <param name="options">The options of the <see cref="KcpRawChannel"/>.</param>
+        /// <returns>The created socket transport instance.</returns>
+        public static IKcpTransport<KcpRawChannel> CreateRawChannel(Socket socket, EndPoint endPoint, int conversationId, KcpRawChannelOptions? options)
+        {
+            if (socket is null)
+            {
+                throw new ArgumentNullException(nameof(socket));
+            }
+            if (endPoint is null)
+            {
+                throw new ArgumentNullException(nameof(endPoint));
+            }
+
+            return new KcpSocketTransportForRawChannel(socket, endPoint, conversationId, options);
+        }
+
+        /// <summary>
+        /// Create a socket transport for raw channel with no conversation ID.
+        /// </summary>
+        /// <param name="socket">The socket instance.</param>
+        /// <param name="endPoint">The remote endpoint.</param>
+        /// <param name="options">The options of the <see cref="KcpRawChannel"/>.</param>
+        /// <returns>The created socket transport instance.</returns>
+        public static IKcpTransport<KcpRawChannel> CreateRawChannel(Socket socket, EndPoint endPoint, KcpRawChannelOptions? options)
+        {
+            if (socket is null)
+            {
+                throw new ArgumentNullException(nameof(socket));
+            }
+            if (endPoint is null)
+            {
+                throw new ArgumentNullException(nameof(endPoint));
+            }
+
+            return new KcpSocketTransportForRawChannel(socket, endPoint, null, options);
+        }
+
+        /// <summary>
+        /// Create a socket transport for multiplex connection.
+        /// </summary>
+        /// <param name="socket">The socket instance.</param>
+        /// <param name="endPoint">The remote endpoint.</param>
+        /// <param name="mtu">The maximum packet size that can be transmitted over the socket.</param>
+        /// <returns></returns>
+        public static IKcpTransport<IKcpMultiplexConnection> CreateMultiplexConnection(Socket socket, EndPoint endPoint, int mtu)
+        {
+            if (socket is null)
+            {
+                throw new ArgumentNullException(nameof(socket));
+            }
+            if (endPoint is null)
+            {
+                throw new ArgumentNullException(nameof(endPoint));
+            }
+
+            return new KcpSocketTransportForMultiplexConnection<object>(socket, endPoint, mtu);
+        }
+
+        /// <summary>
+        /// Create a socket transport for multiplex connection.
+        /// </summary>
+        /// <typeparam name="T">The type of the user state.</typeparam>
+        /// <param name="socket">The socket instance.</param>
+        /// <param name="endPoint">The remote endpoint.</param>
+        /// <param name="mtu">The maximum packet size that can be transmitted over the socket.</param>
+        /// <returns></returns>
+        public static IKcpTransport<IKcpMultiplexConnection<T>> CreateMultiplexConnection<T>(Socket socket, EndPoint endPoint, int mtu)
+        {
+            if (socket is null)
+            {
+                throw new ArgumentNullException(nameof(socket));
+            }
+            if (endPoint is null)
+            {
+                throw new ArgumentNullException(nameof(endPoint));
+            }
+
+            return new KcpSocketTransportForMultiplexConnection<T>(socket, endPoint, mtu);
+        }
+
+        /// <summary>
+        /// Create a socket transport for multiplex connection.
+        /// </summary>
+        /// <typeparam name="T">The type of the user state.</typeparam>
+        /// <param name="socket">The socket instance.</param>
+        /// <param name="endPoint">The remote endpoint.</param>
+        /// <param name="mtu">The maximum packet size that can be transmitted over the socket.</param>
+        /// <param name="disposeAction">The action to invoke when state object is removed.</param>
+        /// <returns></returns>
+        public static IKcpTransport<IKcpMultiplexConnection<T>> CreateMultiplexConnection<T>(Socket socket, EndPoint endPoint, int mtu, Action<T?>? disposeAction)
+        {
+            if (socket is null)
+            {
+                throw new ArgumentNullException(nameof(socket));
+            }
+            if (endPoint is null)
+            {
+                throw new ArgumentNullException(nameof(endPoint));
+            }
+
+            return new KcpSocketTransportForMultiplexConnection<T>(socket, endPoint, mtu, disposeAction);
+        }
+    }
+}
---
+++ GameServer/KcpSharp/KcpSocketTransportForRawChannel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace KcpSharp
+{
+    internal sealed class KcpSocketTransportForRawChannel : KcpSocketTransport<KcpRawChannel>, IKcpTransport<KcpRawChannel>
+    {
+        private readonly int? _conversationId;
+        private readonly KcpRawChannelOptions? _options;
+
+        private Func<Exception, IKcpTransport<KcpRawChannel>, object?, bool>? _exceptionHandler;
+        private object? _exceptionHandlerState;
+
+
+        internal KcpSocketTransportForRawChannel(Socket socket, EndPoint endPoint, int? conversationId, KcpRawChannelOptions? options)
+            : base(socket, endPoint, options?.Mtu ?? KcpConversationOptions.MtuDefaultValue)
+        {
+            _conversationId = conversationId;
+            _options = options;
+        }
+
+        protected override KcpRawChannel Activate() => _conversationId.HasValue ? new KcpRawChannel(this, _conversationId.GetValueOrDefault(), _options) : new KcpRawChannel(this, _options);
+
+        protected override bool HandleException(Exception ex)
+        {
+            if (_exceptionHandler is not null)
+            {
+                return _exceptionHandler.Invoke(ex, this, _exceptionHandlerState);
+            }
+            return false;
+        }
+
+        public void SetExceptionHandler(Func<Exception, IKcpTransport<KcpRawChannel>, object?, bool> handler, object? state)
+        {
+            _exceptionHandler = handler;
+            _exceptionHandlerState = state;
+        }
+    }
+}

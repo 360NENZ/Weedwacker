11dc3f9e40b8209e59f4807685d8d580f2719495
akatatsu27 <xapan2000@yahoo.gr>
2022-09-21 20:50:34 +0300

    some fetter stuff

---
+++ src/GameServer/Data/Excel/FetterCharacterCardData.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Weedwacker.GameServer.Data.Excel
+{
+    [Resource("FetterCharacterCardExcelConfigData.json")]
+    internal class FetterCharacterCardData : GameResource
+    {
+        [JsonProperty]
+        public readonly int avatarId;
+        [JsonProperty]
+        public readonly int fetterLevel;
+        [JsonProperty]
+        public readonly int rewardId;
+    }
+}
---
+++ src/GameServer/Data/Excel/FetterInfoData.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Weedwacker.GameServer.Data.Common;
+
+namespace Weedwacker.GameServer.Data.Excel
+{
+    [Resource("FetterInfoExcelConfigData.json")]
+    internal class FetterInfoData
+    {
+        [JsonProperty]
+        public readonly int infoBirthMonth;
+        [JsonProperty]
+        public readonly int infoBirthDay;
+        [JsonProperty]
+        public readonly long avatarNativeTextMapHash;
+        [JsonProperty]
+        public readonly long avatarVisionBeforTextMapHash;
+        [JsonProperty]
+        public readonly long avatarConstellationBeforTextMapHash;
+        [JsonProperty]
+        public readonly long avatarTitleTextMapHash;
+        [JsonProperty]
+        public readonly long avatarDetailTextMapHash;
+        [JsonProperty]
+        public readonly string avatarAssocType;
+        [JsonProperty]
+        public readonly long cvChineseTextMapHash;
+        [JsonProperty]
+        public readonly long cvJapaneseTextMapHash;
+        [JsonProperty]
+        public readonly long cvEnglishTextMapHash;
+        [JsonProperty]
+        public readonly long cvKoreanTextMapHash;
+        [JsonProperty]
+        public readonly long avatarVisionAfterTextMapHash;
+        [JsonProperty]
+        public readonly long avatarConstellationAfterTextMapHash;
+        [JsonProperty]
+        public readonly int fetterId;
+        [JsonProperty]
+        public readonly int avatarId;
+        [JsonProperty]
+        public readonly FetterOpenCond[] openConds; 
+    }
+}
